64574f16a71670661a0e055f90793eea7d214048
KkarliskK <42585445+KkarliskK@users.noreply.github.com>
2023-09-26 11:36:40 +0300

    v2

--- login.php
+++ login.php
@@ -9,8 +9,25 @@ include '../config/db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login/Register</title>
+    <link rel="stylesheet" href="../public/style.css">
 </head>
 <body>
-    
+    <div class="login-container-box">
+        <div class="login-container">
+            <div class="login-container-header">
+                <h2>Login</h2>
+            </div>
+            <div class="login-container-body">
+                <form method="post">
+                    <input type="text" id="login-username" name="login-username" placeholder="Username">
+                    <input type="password" id="login-password" name="login-password" placeholder="Password">
+                </form>
+                <button id="login-button" name="login-button">Log In</button>
+            </div>
+            <div class="login-container-footer">
+                <p>Don't have an account? </p><a href="#">register</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
